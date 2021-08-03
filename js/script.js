adf446afa2b88d812c3dd3265a0e93707d9acf54
Jan Robert <janmartinrobert@gmail.com>
2021-08-03 15:56:05 +0200

    Add math functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log('it works')
+
+const add = (a, b) => a + b
+
+const multiply = (a, b) => a * b
+
+const factorial = n => (n === 0 ? 1 : n * factorial(n - 1))
+
+console.log('2 and 40 is', add(2, 40))
+console.log('3 times 4 is', multiply(3, 4))
+console.log('factorial of 3 is', factorial(3))
