dcc78a5f769d7f784280a4cfe497831dd4565027
Jan Robert <janmartinrobert@gmail.com>
2021-08-03 16:01:39 +0200

    Refactor to Math module

---
+++ js/math.js
@@ -0,0 +1,5 @@
+export const add = (a, b) => a + b
+
+export const multiply = (a, b) => a * b
+
+export const factorial = n => (n === 0 ? 1 : n * factorial(n - 1))
--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
+import { add, multiply, factorial } from './math'
+
 console.log('it works')
-
-const add = (a, b) => a + b
-
-const multiply = (a, b) => a * b
-
-const factorial = n => (n === 0 ? 1 : n * factorial(n - 1))
 
 console.log('2 and 40 is', add(2, 40))
 console.log('3 times 4 is', multiply(3, 4))
